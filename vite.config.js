485f89fe890dd82e638b6cf8acf29199db933433
Ferenc Csonka <ferenc@saas.group>
2026-04-13 22:09:59 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ⚠️  Update `base` to match your exact GitHub repo name
-// e.g. if your repo is github.com/yourname/my-tool → base: "/my-tool/"
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/ferenc-sg/competency-self-assessment",
+  base: "/competency-self-assessment/",
 });
